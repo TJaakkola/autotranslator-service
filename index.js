2decba5aa038047a3cf4946115b41cbb12cf55f5
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2015-04-05 22:41:52 +0300

    set environment variables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var credentials = {
-    client_id: 'QTranslator',
-    client_secret: 'K0EM7nT7dHFGWZ4lIijNC1IFMtIxQ/HA33PytsTEdYU='
+    client_id: process.env.CLIEN_ID,
+    client_secret: process.env.MS_APP_KEY
 };
 
 var translator = require('bing-translate').init(credentials);
@@ -15,13 +15,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
-translator.translate('This hotel is located close to the centre of Paris.', 'en', 'fi', function(err, res){
-    console.log(res.response);
-});
-
-
 
 var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
