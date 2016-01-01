73c26854940d58d773629ce0bbf0a31387ed4eed
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-01-01 19:51:29 +0200

    refactor to es2015

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ var credentials = {
 };
 
 var translator = require('bing-translate').init(credentials);
-var app = express();
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(function(req, res, next) {
+app.use( (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-var server = app.listen(app.get('port'), function () {
+const server = app.listen(app.get('port'), () => {
     var host = server.address().address;
     var port = server.address().port;
     console.log('autotranslator-service started, running at', app.get('port'), 'on host', host);
 });
 
-app.get('/translate', function (req, res) {
+app.get('/translate', (req, res) => {
     translator.translate(req.query.phrase, req.query.from, req.query.to, function (err, data) {
         console.log(req.query.phrase, req.query.from, req.query.to);
         res.send(data.translated_text);
